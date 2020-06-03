5925b64f55344e475856583d20ec9e560271b91a
Jonathan Santos <silva.santos.jonathan@gmail.com>
2020-06-03 16:44:02 -0300

    Now user can remove all cards

--- commands/remove.py
+++ commands/remove.py
@@ -3,12 +3,17 @@ from model.card import Card
 import peewee
 
 
+
 @click.command()
+@click.argument('removeall', default='', type=click.STRING, required=False)
 @click.option('--cardid', '-id', type=click.INT, default=-1, help='Specify a card to be removed')
 @click.option('--tag', '-t', type=click.STRING, default='', help='Remove all cards of a TAG')
-def remove(cardid, tag):
+def remove(removeall, cardid, tag):
 
-    if (cardid == -1) and (len(tag) < 1):
+    all = False
+    if removeall == 'all': all = True
+
+    if (cardid == -1) and (len(tag) < 1) and (all == False):
         click.echo("Ops! You need to specify a card id or a tag to execute this command!")
         exit(1)
 
@@ -18,5 +23,9 @@ def remove(cardid, tag):
     if len(tag) > 0:
         query = Card.delete().where(Card.tag == tag)
 
+    if all:
+        click.confirm('Are you sure that you want to remove all cards?', abort=True)
+        query = Card.delete()
+
     result = query.execute()
-    click.echo(str(result) + ' card(s) deleted.')
+    click.echo(str(result) + ' card(s) removed.')
