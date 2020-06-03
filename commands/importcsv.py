1d23976f4ef4972aaff3dcb104cefe0eabe18379
Jonathan Santos <silva.santos.jonathan@gmail.com>
2020-06-03 19:37:34 -0300

    Import flashcards from CSV

---
+++ commands/importcsv.py
@@ -0,0 +1,31 @@
+import os
+import csv
+import click
+from model.card import Card
+
+@click.command()
+@click.argument('path')
+@click.option('--frontalcol', '-fc', type=click.INT, default=0, help='Column index of Frontal Face')
+@click.option('--hiddencol', '-hfc', type=click.INT, default=1, help='Column index of Hidden Face')
+@click.option('--tagcol', '-tc', type=click.INT, default=1, help='Column index of TAG')
+@click.option('--delimiter', type=click.STRING, default=',', help='CSV column delimiter character.')
+@click.option('--header', type=click.BOOL, default=False, help='Header (False by default)')
+def importcsv(path, frontalcol, hiddencol, tagcol, delimiter, header):
+    if not os.path.isfile(path):
+
+        exit(1)
+
+    entries = 0
+    with open(path, 'r') as csvinput:
+        csvreader = csv.reader(csvinput, delimiter=delimiter)
+        for index, row in enumerate(csvreader):
+            if header and (index == 0): continue
+            card = Card()
+            card.frontal = row[frontalcol]
+            card.hidden = row[hiddencol]
+            card.tag = row[tagcol]
+            card.save()
+            entries = entries + 1
+
+
+    click.echo(str(entries) + ' card(s) imported')
